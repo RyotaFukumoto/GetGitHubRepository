830bfe849857db2b47596d1ebe379bb7abf86f7c
ryota <k014c1106@it-neec.jp>
2022-09-04 22:45:34 +0900

    add okhttp

--- app/src/main/java/com/example/getgithubrepository/MainActivity.kt
+++ app/src/main/java/com/example/getgithubrepository/MainActivity.kt
@@ -10,5 +10,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val request = Request.Builder()
+            .url(getString(R.string.whetherApi))
+            .build()
+
+        val client = OkHttpClient()
+        client.newCall(request).enqueue(object : Callback {
+            // 成功時に呼ばれる
+            override fun onResponse(call: Call, response: Response) {
+                Log.d("@@@@@debug", response.body!!.string())
+            }
+
+            // 失敗時に呼ばれる
+            override fun onFailure(call: Call, e: IOException) {
+                Log.d("@@@@@debug", e.toString())
+            }
+        })
+
     }
 }
